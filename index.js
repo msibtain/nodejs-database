5049009c49fd04d562606824470c65587ed4a486
Muhammad Sibtain <mohsibtain@gmail.com>
2024-08-13 16:51:04 +0500

    localhost server connectivity

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const Express = require('express');
 const objExpress = new Express();
+const bodyParser = require('body-parser');
 
 const db = require('./includes/dbconnection');
 
-console.log('EOF by sib.');
+objExpress.use(bodyParser.json());
+objExpress.use(bodyParser.urlencoded({ extended: true }));
+
+objExpress.post('/api/login', (req,res) => {
+
+    
+    res.json({
+        'id': 1,
+        'name': 'Sibtain',
+        'username': req.body.username,
+        'password': req.body.password
+    });
+
+});
+
+const port = 3000
+objExpress.listen(port,()=>console.log(`Listening at ${port}...`));
